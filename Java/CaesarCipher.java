e45f6e9e5f3d87e836aa06c3e4bf2eb53d3ef46b
cosin <cosinus.1991@mail.ru>
2026-05-31 15:02:21 +0300

    Создана возможность добавлять буквы из конца алфавита без ошибок в программе

--- Java/CaesarCipher.java
+++ Java/CaesarCipher.java
@@ -1,19 +1,34 @@
 public class CaesarCipher {
-    public static final char[] alphabet = {
+    public static final char[] alphabetRu = {
             'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м',
             'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ',
             'ы', 'ь', 'э', 'ю', 'я'
     };
+
+    public static final char[] alphabetEng = {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
+
 
     public void encryption(String text, int key) {
         int[] keyRealisation = new int[text.length()];
         char[] fragment = new char[text.length()];
         for (int i = 0; i < text.length(); i++) {
             fragment[i] = text.charAt(i);
-            for (int o = 0; o < alphabet.length; o++) {
-                if (fragment[i] == alphabet[o]) {
+            for (int o = 0; o < alphabetRu.length; o++) {
+                if (fragment[i] == alphabetRu[o]) {
                     keyRealisation[i] = o;
                 }
+            }
+        }
+        for (int i = 0; i < keyRealisation.length; i++) {
+            // ИСПРАВЛЕНО: >= вместо >
+            if (keyRealisation[i] + key >= alphabetRu.length) {
+                int solution = keyRealisation[i] + key - alphabetRu.length;
+                System.out.print(alphabetRu[solution]);
+            } else {
+                System.out.print(alphabetRu[keyRealisation[i] + key]);
             }
         }
     }
