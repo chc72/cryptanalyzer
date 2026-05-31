59f0f5ff79e218a582ae8c46bb2698da36f1fdc1
cosin <cosinus.1991@mail.ru>
2026-05-31 14:34:09 +0300

    Создано шифрование объекта

---
+++ Java/CaesarCipher.java
@@ -0,0 +1,26 @@
+public class CaesarCipher {
+    public static final char[] alphabet = {
+            'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м',
+            'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ',
+            'ы', 'ь', 'э', 'ю', 'я'
+    };
+
+    public void encryption(String text, int key) {
+        int[] keyRealisation = new int[text.length()];
+        char[] fragment = new char[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            fragment[i] = text.charAt(i);
+            for (int o = 0; o < alphabet.length; o++) {
+                if (fragment[i] == alphabet[o]) {
+                    keyRealisation[i] = o;
+                }
+            }
+        }
+        for (int i = 0; i<keyRealisation.length; i++)
+        {
+            System.out.print(alphabet[keyRealisation[i]+key]);
+        }
+    }
+}
+
+
