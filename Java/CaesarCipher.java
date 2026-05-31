a22b1ea26e2a6d7b155c7ceb7e4022b0e5c3db6d
cosin <cosinus.1991@mail.ru>
2026-05-31 15:23:20 +0300

    Добавлена расшифровка текста

--- Java/CaesarCipher.java
+++ Java/CaesarCipher.java
@@ -31,6 +31,7 @@ public class CaesarCipher {
             }
         }
     }
+
     public void encryptionEng(String text, int key) {
         int[] keyRealisation = new int[text.length()];
         char[] fragment = new char[text.length()];
@@ -51,6 +52,48 @@ public class CaesarCipher {
             }
         }
     }
+
+    public void decryptRu(String text, int key) {
+        int[] keyRealisation = new int[text.length()];
+        char[] fragment = new char[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            fragment[i] = text.charAt(i);
+            for (int o = 0; o < alphabetRu.length; o++) {
+                if (fragment[i] == alphabetRu[o]) {
+                    keyRealisation[i] = o;
+                }
+            }
+        }
+        for (int i = 0; i < keyRealisation.length; i++) {
+            if (keyRealisation[i] - key < 0) {
+                int solution = keyRealisation[i] - key + alphabetRu.length;
+                System.out.print(alphabetRu[solution]);
+            } else {
+                System.out.print(alphabetRu[keyRealisation[i] - key]);
+            }
+        }
+    }
+    public void decryptEng(String text, int key) {
+        int[] keyRealisation = new int[text.length()];
+        char[] fragment = new char[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            fragment[i] = text.charAt(i);
+            for (int o = 0; o < alphabetEng.length; o++) {
+                if (fragment[i] == alphabetEng[o]) {
+                    keyRealisation[i] = o;
+                }
+            }
+        }
+        for (int i = 0; i < keyRealisation.length; i++) {
+            if (keyRealisation[i] - key < 0) {
+                int solution = keyRealisation[i] - key + alphabetEng.length;
+                System.out.print(alphabetEng[solution]);
+            } else {
+                System.out.print(alphabetEng[keyRealisation[i] - key]);
+            }
+        }
+    }
 }
+
 
 
